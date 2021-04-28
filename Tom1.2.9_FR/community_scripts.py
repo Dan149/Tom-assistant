2171d003c4faad8e7ba68be2c964c40e658d6b18
Daniel <48863749+Dan149@users.noreply.github.com>
2021-04-28 09:42:46 +0200

    Add files via upload

---
+++ Tom1.2.9_FR/community_scripts.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import webbrowser
+import sys
+import os
+import time
+
+# Les fonctions de la communauté peuvent être écrites ici afin de rendre le code plus lisible.
+# ATTENTION: ne pas importer les fichiers 'script.py' ou 'tomodules.py' dans ce fichier.
+
+def example():				
+	print("Hello world !")
+						   
+def browser():
+	print("Opening your default web browser...")
+	webbrowser.open_new("https://www.google.com")
+
+# Modules recommandés :
+	# pygame
+	# sys
+	# hashlib
+	# termcolor
+	# argparse
