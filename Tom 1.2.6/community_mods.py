b6b316453758cef5b89288d6c8c3e08cce49f19c
Daniel <48863749+Dan149@users.noreply.github.com>
2021-04-28 09:35:48 +0200

    Delete Tom 1.2.6 directory

--- Tom 1.2.6/community_mods.py
+++
@@ -1,5 +0,0 @@
-#The new functions of the community can be written here, it will make the code easier to read, and the bugs faster to correct.
-#WARNING: don't import 'script.py' or 'tomodules.py' in this file !
-
-def exemple():				#You can access to those functions in the 'script.py' and 'tomodules.py' files by writting community_mods.exemple().
-	print("Hello world !")
